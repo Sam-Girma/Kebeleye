59b2ba6db040cc404eaad0a7234c926c905475cf
unknown <henokmekuanint79@gmail.com>
2022-03-27 11:26:22 +0300

    materialadded

---
+++ kebeleyeapp/lib/materials/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color blueColors = Color(0xFF2196F3);
+Color bluelightcolor = Color(0xFF03A9F4);
+Color orange = Color(0xffF5591F);
+
+class coloringclasss {
+  static const iconcolor = Color(0xFFB6C7D1);
+  static const Activecolor = Color(0xFF09126C);
+  static const TEXTCOLOR1 = Color(0xFFA7BCC7);
+  static const textcolor2 = Color(0xFF9BB3C0);
+  static const facebookcolor = Color(0xFF385999);
+  static const googlecolor = Color(0xFFDE4839);
+  static const backgroundcolor = Color(0xFFECF3F9);
+}
