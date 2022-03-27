332d5bb58f7b01b215761feb56d56b354edcff17
unknown <henokmekuanint79@gmail.com>
2022-03-27 11:36:26 +0300

    logindartadded

---
+++ kebeleyeapp/lib/pages/mystate.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kebeleyeapp/materials/colors.dart';
+
+
+class mystate extends StatefulWidget {
+  @override
+  State<mystate> createState() => _mycurrentstate();
+}
+
+class _mycurrentstate extends State<mystate> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(milliseconds: 4000), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => //LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [blueColors, bluelightcolor],
+                  end: Alignment.bottomCenter,
+                  begin: Alignment.topCenter),
+            ),
+            child: Center(child: Image.asset("assets/logo9.png"))));
+  }
+}
