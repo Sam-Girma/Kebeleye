5de3cc412496b5a54a8f85f7de99b62b4b63b616
Joshua Tesfaye <jt272525@gmail.com>
2022-05-30 23:48:12 +0300

    correcting-2

---
+++ DjangoBackend/accounts/models.py
@@ -0,0 +1,56 @@
+from datetime import timezone
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager, AbstractBaseUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from DjangoBackend.tracker.models import TrackingModel
+
+
+# Create your models here.
+
+
+class Account(AbstractBaseUser, PermissionsMixin, TrackingModel):
+    """
+   An abstract base class implementing a fully featured User model with
+   admin-compliant permissions.
+
+   Username and password are required. Other fields are optional.
+   """
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        ("username"),
+        max_length=150,
+        unique=True,
+        help_text=(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": ("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), blank=True)
+    is_staff = models.BooleanField(
+        ("staff status"),
+        default=False,
+        help_text=(
+            "Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        ("active"),
+        default=True,
+        help_text=(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(("date joined"), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
